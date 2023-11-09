fdd2cf28a2af5dd16141cc38e6005012646bc219
carlospuigserver <cpuiggut@myuax.com>
2023-11-09 16:12:23 +0100

    comit

--- cliente.py
+++ cliente.py
@@ -242,52 +242,44 @@ class Cliente:
         return builder.obtener_presentacion()
     
     def elegir_extras(self, builder):
-        quiere_borde_relleno = input("¿Desea su borde relleno? (Sí/No): ")
-        if quiere_borde_relleno.lower() == "sí":
-            print("¿Qué borde relleno desea?")
-            print("1. Relleno de queso")
-            print("2. Ajo y queso parmesano")
-            print("3. Crust de queso")
-            print("4. Relleno de pepperoni o jamón")
-            print("5. Relleno de verduras")
+        opciones_borde = {
+            "1": "Relleno de queso",
+            "2": "Ajo y queso parmesano",
+            "3": "Crust de queso",
+            "4": "Relleno de pepperoni o jamón",
+            "5": "Relleno de verduras"
+        }
 
-            borde = input("Su elección (borde relleno): ")
-            opciones_borde = {
-                "1": "Relleno de queso",
-                "2": "Ajo y queso parmesano",
-                "3": "Crust de queso",
-                "4": "Relleno de pepperoni o jamón",
-                "5": "Relleno de verduras"
-            }
-            builder.anadir_borde_relleno(opciones_borde.get(borde, ""))
+        opciones_ingredientes = {
+            "1": "Trufas",
+            "2": "Caviar",
+            "3": "Foie Gras",
+            "4": "Jamón Iberico",
+            "5": "Quesos exclusivos",
+            "6": "Setas silvestres",
+            "7": "Mariscos de alta calidad",
+            "8": "Verduras orgánicas y raras"
+        }
+
+        quiere_borde_relleno = input("¿Desea su borde relleno? (Sí/No): ")
+        borde_elegido = opciones_borde.get(input("Su elección (borde relleno) (numero 1-5) : "), "")
 
         quiere_ingredientes_gourmet = input("¿Desea algún ingrediente gourmet? (Sí/No): ")
-        if quiere_ingredientes_gourmet.lower() == "sí":
-            print("¿Qué ingrediente gourmet desea?")
-            print("1. Trufas")
-            print("2. Caviar")
-            print("3. Foie Gras")
-            print("4. Jamón Ibérico")
-            print("5. Quesos exclusivos")
-            print("6. Setas silvestres")
-            print("7. Mariscos de alta calidad")
-            print("8. Verduras orgánicas y raras")
+        ingredientes_elegidos = []
+        
+        while quiere_ingredientes_gourmet.lower() == "sí":
+            ingrediente = input("Su elección (ingrediente gourmet) (numero 1-8): ")
+            ingrediente_elegido = opciones_ingredientes.get(ingrediente, "")
+            if ingrediente_elegido:
+                ingredientes_elegidos.append(ingrediente_elegido)
+            else:
+                print("Opción de ingrediente no válida.")
+            quiere_ingredientes_gourmet = input("¿Desea otro ingrediente gourmet? (Sí/No): ")
 
-            ingrediente = input("Su elección (ingrediente gourmet): ")
-            opciones_ingredientes = {
-                "1": "Trufas",
-                "2": "Caviar",
-                "3": "Foie Gras",
-                "4": "Jamón Ibérico",
-                "5": "Quesos exclusivos",
-                "6": "Setas silvestres",
-                "7": "Mariscos de alta calidad",
-                "8": "Verduras orgánicas y raras"
-            }
-            builder.anadir_ingredientes_gourmet(opciones_ingredientes.get(ingrediente, ""))
-
-        # Se retorna el objeto con los extras seleccionados
+        builder.anadir_borde_relleno(borde_elegido)
+        builder.anadir_ingredientes_gourmet(ingredientes_elegidos)
         return builder.obtener_extras()
+
 
     
 
@@ -333,8 +325,7 @@ if __name__ == "__main__":
                          print(f"El usuario {nombre_usuario} ha elegido la presentación {presentacion_elegida.tipo}")
                         
                         extras_elegidos = cliente.elegir_extras(extras_builder)
-                        if extras_elegidos:
-                         print(f"El usuario {nombre_usuario} ha elegido los siguientes extras:{extras_elegidos}")
+                        print(f"Elegidos extras: {extras_elegidos}")
                          
                             
                     # Guardar los detalles del pedido en un archivo CSV (pizzas.csv)
